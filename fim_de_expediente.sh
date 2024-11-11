82ea04db9c3a23269a916497074ca0b316f3699e
budkee <kae.budke@gmail.com>
2024-11-11 17:37:14 -0400

    Organização Repo | #02

--- fim_de_expediente.sh
+++ fim_de_expediente.sh
@@ -1,11 +1,11 @@
 #!/bin/zsh
 # 
-# Repositório mae no GitHub: https://github.com/budkee/estudos
+# Repositório mae no GitHub: https://github.com/budkee/gestao_emergencias
 #
 # Alterar a mensagem conforme o que foi feito no dia 
 
 git add .
-git commit -m "Ajustes | #01"
+git commit -m "Organização Repo | #02"
 echo "\n"
 git push
 echo "\n"
